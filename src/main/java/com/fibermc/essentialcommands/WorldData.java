38bfa561d99dcd3fbf0a7778601a5dfc1250abef
John Paul R <jp@jpcode.dev>
2025-07-14 22:34:45 -0400

    fix: rm 'spawn' if empty object
    by far the most complicated way I've ever done nbt.remove(key)

--- src/main/java/com/fibermc/essentialcommands/WorldData.java
+++ src/main/java/com/fibermc/essentialcommands/WorldData.java
@@ -3,15 +3,18 @@ package com.fibermc.essentialcommands;
 import java.util.Optional;
 
 import com.fibermc.essentialcommands.codec.Codecs;
+import com.fibermc.essentialcommands.datafixer.WorldDataDataFixer;
 import com.fibermc.essentialcommands.types.MinecraftLocation;
 import com.fibermc.essentialcommands.types.WarpStorage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.Dynamic;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtOps;
 
 public class WorldData {
@@ -41,13 +44,24 @@ public class WorldData {
     }
 
     public static WorldData fromNbt(NbtCompound nbt) {
+        nbt = WorldDataDataFixer.createDataFixer().build().fixer().update(
+            WorldDataDataFixer.TYPE,
+            new Dynamic<NbtElement>(NbtOps.INSTANCE, nbt),
+            nbt.getInt(SCHEMA_VERSION_KEY, 0),
+            WorldData.SCHEMA_VERSION
+        ).getValue().asCompound().orElseThrow();
+
         return CODEC.parse(NbtOps.INSTANCE, nbt).getOrThrow();
     }
 
     public NbtCompound toNbt() {
-        return CODEC.encodeStart(NbtOps.INSTANCE, this).getOrThrow().asCompound().orElseThrow();
+        var data = CODEC.encodeStart(NbtOps.INSTANCE, this).getOrThrow().asCompound().orElseThrow();
+        data.putInt(SCHEMA_VERSION_KEY, SCHEMA_VERSION);
+        return data;
     }
 
+    private static final String SCHEMA_VERSION_KEY = "_sv";
+    private static final int SCHEMA_VERSION = 1;
     public static final Codec<WorldData> CODEC = RecordCodecBuilder.create(instance ->
         instance.group(
             Codecs.MINECRAFT_LOCATION.optionalFieldOf("spawn").forGetter(WorldData::getSpawn),
